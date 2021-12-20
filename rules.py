ffafa99263749d8c1bc03d78dfe8f665ef97279e
Adrian Wan <adrianwan2@gmail.com>
2021-12-19 16:14:57 -0800

    Rules implementation with unit tests
    Fix #2

--- rules.py
+++ rules.py
@@ -1,34 +1,47 @@
 """
 Implements the rules of the MU puzzle
 """
+import utils
 
-def rule1(start_str: str) -> list[str]:
+def rule1(s: str) -> list[str]:
   """
   You can add a U to any string ending in I: xI --> xIU
   """
-  # TODO: implement
+  if (s.endswith('I')):
+    return [s + 'U']
   return []
 
-def rule2(start_str: str) -> list[str]:
+def rule2(s: str) -> list[str]:
   """
   If you have Mx, you can create Mxx: Mx --> Mxx
   """
-  # TODO: implement
+  if (s.startswith('M') and len(s) > 1):
+    x = s[1:]
+    return ['M' + x + x]
   return []
 
-def rule3(start_str: str) -> list[str]:
+def rule3(s: str) -> list[str]:
   """
   You can replace III with U: xIIIy --> xUy
   """
-  # TODO: implement
-  return []
+  iii_indices = utils.find_indices_of_substring(s, 'III')
+  def replace_3_chars_with_u_at_index(idx: int) -> str:
+    return s[:idx] + 'U' + s[idx+3:]
+  return [replace_3_chars_with_u_at_index(i) for i in iii_indices]
 
-def rule4(start_str: str) -> list[str]:
+def rule4(s: str) -> list[str]:
   """
   You can delete UU from a string: xUUy --> xy
   """
-  # TODO: implement
-  return []
+  # It's possible that it's not necessary to use lookahead here, i.e. it's not
+  # necessary to worry about overlaps, because `UUU` will convert to `U`
+  # regardless of which `UU` is replaced. For convenience though, re-use this
+  # method and then dedupe the return.
+  uu_indices = utils.find_indices_of_substring(s, 'UU')
+  def delete_2_chars_at_index(idx: int) -> str:
+    return s[:idx] + s[idx+2:]
+  # Deduplicate return
+  return list(set(delete_2_chars_at_index(i) for i in uu_indices))
 
 # Provide a way to get all the rules in one object
 rules = [rule1, rule2, rule3, rule4]
